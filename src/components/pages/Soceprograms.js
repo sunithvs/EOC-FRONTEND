4b649e34055cb37909d935a52a50b7217c5dbe58
Varsha Shaheen <varshashaheen2003@gmail.com>
2022-08-11 12:57:56 +0530

    ghdhjef

--- src/components/pages/Soceprograms.js
+++ src/components/pages/Soceprograms.js
@@ -21,7 +21,21 @@ function SocePrograms() {
                 Centre will conduct Soft Skill Development programmes benefitted for SC/ST Engineering students of
                 CUSAT. The Centre is established in the year 2019 for a period of 3 years. The scheme is beneficial for
                 SC/ST Engineering students to empower and reorient themselves in the current merging opportunities.</p>
-            <h5>Recent Acitivties: </h5>
+            <h3>Upcoming Project </h3>
+            <h4>XR Lab Cusat</h4>
+            <p>Extended reality lab in the Skill Orientation Centre for Engineering (SOCE), CUSAT as its extension
+                proposed by Prof(Dr.) Sasi Gopalan, Department of mathematics (  Mentor & Coordinator SOCE) aims
+                at prototyping human interaction to make the future more better and human centred by creating
+                advancements in computer vision, augmented reality and virtual reality. The advancements
+                in this field indicate that the XR lab will be the next pervasive platform for communication,
+                experimentation, and work via the replacement of current hardware into a Virtual, Augmented,
+                and Mixed reality Library. Our goal is to develop innovative, impactful research and applications
+                in VR, AR and CV by establishing collaborations with industry partners and academia.
+                With pioneering research, XRC will find new ways through invention and innovation to
+                make an impact today while paving the way for the future.
+
+
+</p>
 
             {!programs && <h4>Loading...</h4>}
             {programs.map(({name, longDesc, image, images}) => (
