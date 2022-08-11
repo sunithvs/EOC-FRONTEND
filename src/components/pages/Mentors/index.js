d34c69856f9a8b75bf21fe360c3796c56745a442
Varsha Shaheen <varshashaheen2003@gmail.com>
2022-08-11 13:36:52 +0530

    area of expertise

--- src/components/pages/Mentors/index.js
+++ src/components/pages/Mentors/index.js
@@ -1,14 +1,14 @@
 import {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 
-function MentorCard({name, designation, image, expertise}) {
+function MentorCard({name, designation, image, area_or_expertise}) {
     return (
         <div className="card m-1" style={{width: "15rem"}}>
             <img className="card-img-top" src={image} style={{aspectRatio: "1"}} alt={name}/>
             <div className="card-body">
                 <h5 className="card-title">{name}</h5>
                 <p className="card-text">{designation}</p>
-                <p className="card-text">{expertise}</p>
+                <p className="card-text">{area_or_expertise}</p>
             </div>
         </div>
     );
@@ -36,7 +36,7 @@ export default function Mentors() {
                     <h4>{t}</h4>
                     <div className="d-flex flex-row justify-content-evenly flex-wrap">
                         {mentors.filter(({type}) => t === type).map(({id, name, designation, image, area_or_expertise}) =>
-                            <MentorCard designation={designation} name={name} image={image} key={id} expertise={ area_or_expertise}/>
+                            <MentorCard designation={designation} name={name} image={image} key={id} area_or_expertise={ area_or_expertise}/>
                         )}
                     </div>
                     <hr/>
